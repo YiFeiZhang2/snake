c0081811f870d71d6eecf337ad06d2d5c091c7db
Richard Zhang <richardzhang97@gmail.com>
2017-01-08 16:35:52 -0500

    Create foodModule for food

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,48 @@ var canvas = document.getElementById("backgroundCanvas");
 canvas.width = 750;
 canvas.height = 500;
 
+var foodModule = (function(canvas, snakeModule){
+    var food_arr = []; //array of food_arr
+
+    function Food(){
+        this.colour = "#ffffff";
+        this.posx = Math.random()*(canvas.width);
+        this.posy = Math.random()*(canvas.height);
+        this.size = 3;
+    };
+
+    function drawFood(food, context){
+        context.fillStyle = food.colour;
+        context.beginPath();
+        context.arc(food.posx, food.posy, food.size, 0, 2*Math.PI);
+        context.closePath();
+        context.fill();
+        return;
+    };
+
+    function drawAllFood(context){
+        for (i = 0; i<food_arr.length; i++){
+            drawFood(food_arr[i], context);
+        }
+        return;
+    }
+
+    return {
+        init: function(num_snakes){
+            if (canvas.getContext)
+                var ctx = canvas.getContext('2d');
+            else return;
+
+            for (i = 0; i<num_snakes; i++){
+                var food = new Food();
+                food_arr.push(food);
+            }
+
+            drawAllFood(ctx);
+        },
+    }
+})(canvas, snakeModule);
+
 var snakeModule = (function(canvas) {
 
     var snake_arr = []; //array of snakes
@@ -18,16 +60,29 @@ var snakeModule = (function(canvas) {
     }
 
     function Snake(num) {
+        this.isAI = false;
         this.colour = colour[num];
         this.length = 1;
         this.body = [ generateHead() ];
+        this.size = 7.5;
     }
 
     function drawSnake(snake, context){
-        for (i = 0; i < snake.length; i++){
-            context.fillStyle = "#ffffff";//snake.colour;
-            context.arc(snake.body[i].posx, snake.body[i].posy, 7.5, 0, 2*Math.PI);
+        for (j = 0; j < snake.length; j++){
+            context.fillStyle = snake.colour;
+            context.beginPath();
+            context.arc(snake.body[j].posx, snake.body[j].posy, snake.size, 0, 2*Math.PI);
+            context.closePath();
             context.fill();
+        }
+        return;
+    }
+
+    function calcPos(){
+        for (i = 0; i < snake_arr.length; i++){
+            for (j = 0; j < snake_arr[i].body.length; j++){
+                
+            }
         }
     }
 
@@ -35,13 +90,17 @@ var snakeModule = (function(canvas) {
         context.fillStyle = "#000000";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        for (i = 0; i < snake_arr.length; i++){   
+        for (i = 0; i < snake_arr.length; i++){
             drawSnake(snake_arr[i], context);
         }
+        return;
+    };
+
+    function getNumSnake(){
+        return snake_arr.length;
     };
 
     return {
-
         init: function(num_snakes){
             if (canvas.getContext)
                 context = canvas.getContext('2d');
@@ -49,15 +108,19 @@ var snakeModule = (function(canvas) {
 
             for (var i = 0; i < num_snakes; i++){
                 var snake = new Snake(i);
-                
-                alert(snake.body[0].posy);
-
                 snake_arr.push(snake);
             }
 
             drawFrame(context);
         },
+
+        numSnake: function(){
+            return getNumSnake();
+        }
     };
 })(canvas);
 
-snakeModule.init(1);
+var num_snake = 1;
+
+snakeModule.init(num_snake);
+foodModule.init(num_snake);
